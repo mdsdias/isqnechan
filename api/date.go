2c2cd11d1a0cfc29bfed2f07fbb3b59faaebe5e0
Isqne <79223936+Isqneeh@users.noreply.github.com>
2021-07-14 08:15:21 -0300

    NaN

--- api/date.go
+++ api/date.go
@@ -7,6 +7,6 @@ import (
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	currentTime := time.Now().Format(time.RFC850).UTC()
+	currentTime := time.Now().Format(time.RFC850).UTC(+3)
 	fmt.Fprintf(w, currentTime)
 }
