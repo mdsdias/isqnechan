ad049091c7b51a14306146e8d9db886bf5e6f290
Isqneeh <isqneimortal@gmail.com>
2021-09-01 20:15:31 -0300

    testes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <body>
-      <main>
+      <main align='left'>
         <h2>Isqneeh</h2>
         <br />
         <h3>repo:</h3><code>Isqneeh/isqnechan</code>
