7c20123fae38679b82b40f13b705e9e9a153ad83
Isqneeh <isqneimortal@gmail.com>
2021-09-01 19:17:14 -0300

    a

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   return (
     <main>
-      <h2>Bem vindo ao IsqneSite!<h2>
+      <h2>Bem vindo ao IsqneSite!</h2>
       <h3>É isso por agora :D</h3>
     </main>;
   )
