ddd319ffc73b3fe64d5d7041f0bce8cc2b238dd7
Isqneeh <isqneimortal@gmail.com>
2021-09-10 20:04:07 -0300

    kk random d+++

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ def __init__():
     load = {
         "pro": [
             {"name": "GrabberTool", 
-             "url":"http://github.com/Isqneeh/GrabberTool"
+             "url":"http://github.com/Isqneeh/GrabberTool",
+             "desc":"Uma ferramenta feita para puxar dados de pessoas fisicas."
              }
         ]
     }
