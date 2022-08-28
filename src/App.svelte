b2d71eabe6086ed026f433c993b30f35c7bc08a9
miguel <isqne@pm.me>
2022-08-28 17:57:01 +0000

    Working

--- src/App.svelte
+++ src/App.svelte
@@ -16,27 +16,25 @@
   let src = "https://avatars.githubusercontent.com/u/79223936?v=4";
 </script>
 
-<div class="font-sans flex grid">
-  <h1 class="text-4xl tracking-tighter font-light" align="center">Hello World, I am Isqne!</h1>
-  <div class="inline-flex items-center" align="middle">
-    <img alt="img" {src} class="avatar" />
-    {#each midias as get}
-      <div class="midias">
-        <h2><a href={get.url} class="font-semibold text-2xl" target="_blank">{get.name}</a></h2>
-      </div>
-    {/each}
+<div class="font-sans flex flex-col space-y-50 items-center">
+  <h1 class="text-4xl tracking-tighter font-light">
+    Hello World, I am Isqne!
+  </h1>
+  <div class="flex-auto items-center">
+    <div class="flex h-600 w-600">
+      <span
+        class="animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75"
+      />
+      <span class="relative inline-flex bg-sky-500 w-500 h-500"
+        ><img alt="img" {src} class="rounded-full" /></span
+      >
+    </div>
+    <div class="flex flex-col">
+      {#each midias as get}
+        <h2 class="text-4xl tracking-tighter font-light bg-sky-500 w-auto">
+          <a href={get.url} target="_blank">{get.name}</a>
+        </h2>
+      {/each}
+    </div>
   </div>
 </div>
-
-<style>
-  .midias {
-    display: inline-table;
-    padding: 2rem;
-  }
-
-  .avatar {
-    width: 200px;
-    height: auto;
-    border-radius: 50%;
-  }
-</style>
