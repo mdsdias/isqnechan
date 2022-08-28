e0f72103bd4de00202398172bbaed597f6567740
miguel <isqne@pm.me>
2022-08-28 17:16:58 +0000

    Atualizando a bagaça kakak

--- src/App.svelte
+++ src/App.svelte
@@ -2,34 +2,38 @@
   let midias = [
     {
       name: "Twitter",
-      id: "@Legumiih",
       url: "https://twitter.com/Legumiih",
     },
     {
       name: "Github",
-      id: "Legumii",
       url: "https://github.com/legumii",
     },
     {
       name: "Discord",
-      id: "legumi#0719",
       url: "https://discord.com/users/783058100482867234",
     },
   ];
   let src = "https://avatars.githubusercontent.com/u/79223936?v=4";
 </script>
 
-<div class="slides flex-slider flex-container" align="middle">
-  <h1 class="clearfix" align="center">Hello World, I am Isqne!</h1>
-  <img alt="img" {src} class="avatar flexslider" />
-  {#each midias as get}
-    <div class="flex-container">
-      <h2>{get.name}</h2>
-      <h2><a href={get.url} target="_blank">{get.id}</a></h2>
-    </div>
-  {/each}
+<div class="font-sans flex grid">
+  <h1 class="text-4xl tracking-tighter font-light" align="center">Hello World, I am Isqne!</h1>
+  <div class="inline-flex items-center" align="middle">
+    <img alt="img" {src} class="avatar" />
+    {#each midias as get}
+      <div class="midias">
+        <h2><a href={get.url} class="font-semibold text-2xl" target="_blank">{get.name}</a></h2>
+      </div>
+    {/each}
+  </div>
 </div>
+
 <style>
+  .midias {
+    display: inline-table;
+    padding: 2rem;
+  }
+
   .avatar {
     width: 200px;
     height: auto;
