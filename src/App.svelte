850a00fff252e81dd52de7af61f615f3852155aa
miguel <isqne@pm.me>
2022-09-03 04:01:25 +0000

    ...

--- src/App.svelte
+++ src/App.svelte
@@ -16,20 +16,126 @@
   let src = "https://avatars.githubusercontent.com/u/79223936?v=4";
 </script>
 
-<div class="font-sans grid">
-  <h1 class="text-4xl tracking-tighter font-light">
-    Hello World, I am Isqne!
-  </h1>
-  <div class="flex-auto items-center">
-    <div class="flex h-auto w-400">
-      <img alt="img" {src} class="rounded-full" />
+<div class="flex justify-start items-start w-full h-full bg-white">
+  <div
+    class="flex justify-center items-center self-stretch flex-grow-0 flex-shrink-0 gap-2.5 px-[45px] py-[97px] bg-gradient-to-b from-[#1f0a42] to-[#7b2cba]/75"
+  >
+    <div
+      class="flex flex-col justify-start items-center flex-grow-0 flex-shrink-0 relative gap-[30px]"
+    >
+      <img
+        src="assets/img/rectangle-1.png"
+        alt="av"
+        class="flex-grow-0 flex-shrink-0 w-[350px] h-[440px] rounded-[49px] object-cover"
+      />
+      <p
+        class="flex-grow-0 flex-shrink-0 w-[172px] h-[84.54px] text-[50px] font-thin text-center text-white"
+      >
+        Legumii
+      </p>
+      <div class="flex-grow-0 flex-shrink-0 w-[116px] h-[199.18px]">
+        <p
+          class="w-[100px] h-[58.75px] absolute left-[125px] top-[584.54px] text-[35px] font-thin text-center text-white"
+        >
+          Github
+        </p>
+        <p
+          class="w-[106px] h-[58.75px] absolute left-[122px] top-[654.76px] text-[35px] font-thin text-center text-white"
+        >
+          Twitter
+        </p>
+        <p
+          class="w-[116px] h-[58.75px] absolute left-[117px] top-[724.97px] text-[35px] font-thin text-center text-white"
+        >
+          Discord
+        </p>
+      </div>
     </div>
-    <div class="flex flex-col items-center">
-      {#each midias as get}
-        <h2 class="text-4xl tracking-tighter font-light">
-          <a href={get.url} target="_blank">{get.name}</a>
-        </h2>
-      {/each}
+  </div>
+  <div
+    class="flex flex-col justify-start items-start self-stretch flex-grow relative overflow-hidden gap-2.5 p-2.5 bg-white"
+  >
+    <div class="flex-grow-0 flex-shrink-0 w-[1168px] h-[281px]">
+      <div
+        class="flex justify-start items-center w-[1168px] absolute left-2.5 top-[79px] gap-2.5 py-1.5"
+      >
+        <div
+          class="flex justify-start items-start flex-grow-0 flex-shrink-0 w-[300px] h-[200px] relative overflow-hidden gap-2.5 rounded-[25px] bg-white"
+          style="box-shadow: 0px 0px 6px 2px rgba(0,0,0,0.25);"
+        >
+          <img
+            src="assets/img/rectangle-3-2.png"
+            alt="westing"
+            class="self-stretch flex-grow object-cover"
+          />
+          <p
+            class="flex-grow-0 flex-shrink-0 text-[50px] font-thin text-center text-white"
+          >
+            westing
+          </p>
+        </div>
+        <div
+          class="flex justify-start items-start flex-grow-0 flex-shrink-0 w-[300px] relative overflow-hidden gap-2.5 rounded-[25px] bg-white"
+          style="box-shadow: 0px 4px 4px 0 rgba(0,0,0,0.25);"
+        >
+          <img
+            src="assets/img/rectangle-3.png"
+            alt="tob"
+            class="flex-grow h-[200px] object-cover"
+          />
+          <p
+            class="flex-grow-0 flex-shrink-0 text-[50px] font-thin text-center text-white"
+          >
+            tob
+          </p>
+        </div>
+        <div
+          class="flex justify-start items-start flex-grow-0 flex-shrink-0 w-[300px] h-[200px] relative overflow-hidden gap-2.5 rounded-[25px] bg-white"
+          style="box-shadow: 0px 0px 6px 2px rgba(0,0,0,0.25);"
+        >
+          <img
+            src="assets/img/rectangle-3-3.png"
+            alt="grabber"
+            class="self-stretch flex-grow object-cover"
+          />
+          <p
+            class="flex-grow-0 flex-shrink-0 text-[50px] font-thin text-center text-white"
+          >
+            GrabberTool
+          </p>
+        </div>
+      </div>
+      <p
+        class="absolute left-2.5 top-2.5 text-[50px] font-thin text-center text-black"
+      >
+        Projetos
+      </p>
+    </div>
+    <div class="flex-grow-0 flex-shrink-0 w-[595px] h-[98px]">
+      <p
+        class="absolute left-2.5 top-[370px] text-[25px] font-thin text-center text-black"
+      >
+        Um garoto querendo aprender sobre um pouco de tudo
+      </p>
+      <p
+        class="absolute left-2.5 top-[301px] text-[50px] font-thin text-center text-black"
+      >
+        Sobre mim
+      </p>
+    </div>
+    <div class="flex-grow-0 flex-shrink-0 w-[110px] h-[168px]">
+      <div
+        class="flex justify-start items-center absolute left-2.5 top-[477px] gap-[15px]"
+      >
+        <div
+          class="flex-grow-0 flex-shrink-0 w-[100px] h-[100px] relative overflow-hidden rounded-[25px] bg-black/20"
+        />
+      </div>
+      <p
+        class="absolute left-2.5 top-[409px] text-[50px] font-thin text-center text-black"
+      >
+        Skills
+      </p>
     </div>
   </div>
 </div>
