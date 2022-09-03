a37c16c9645c6aea43ef93c44c812e2016901c94
miguel <isqne@pm.me>
2022-09-03 01:28:56 +0000

    ...

--- src/App.svelte
+++ src/App.svelte
@@ -16,22 +16,17 @@
   let src = "https://avatars.githubusercontent.com/u/79223936?v=4";
 </script>
 
-<div class="font-sans flex flex-col space-y-50 items-center">
+<div class="font-sans grid">
   <h1 class="text-4xl tracking-tighter font-light">
     Hello World, I am Isqne!
   </h1>
   <div class="flex-auto items-center">
-    <div class="flex h-600 w-600">
-      <span
-        class="animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75"
-      />
-      <span class="relative inline-flex bg-sky-500 w-500 h-500"
-        ><img alt="img" {src} class="rounded-full" /></span
-      >
+    <div class="flex h-auto w-400">
+      <img alt="img" {src} class="rounded-full" />
     </div>
-    <div class="flex flex-col">
+    <div class="flex flex-col items-center">
       {#each midias as get}
-        <h2 class="text-4xl tracking-tighter font-light bg-sky-500 w-auto">
+        <h2 class="text-4xl tracking-tighter font-light">
           <a href={get.url} target="_blank">{get.name}</a>
         </h2>
       {/each}
